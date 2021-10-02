03031fbf67bab847e9add46cac1e2eda3026920a
IliaStepenko <stepenkoila@gmail.com>
2021-10-02 16:45:52 +0300

    crud methods

--- botadmin/db/config.py
+++ botadmin/db/config.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from datasource import DBConfig
+from .datasource import DBConfig
 
 engine = create_async_engine(DBConfig.get_ass_db_url(), future=True, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
